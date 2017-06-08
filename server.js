3d9a5ee1ae25185979a0eec77692f38d850b7eaa
Nicholas Kajoh <kajohterna@gmail.com>
2017-06-08 15:45:39 +0100

    refactor code

--- server.js
+++ server.js
@@ -17,71 +17,71 @@ var db = new Db('expressBlog', server);
 var ObjectId = mongo.ObjectID;
 
 db.open((err, db) => {
-	if(!err) {
-		app.listen(3000, () => {
-			console.log("Running on http://localhost:3000");
-		});
+  if(!err) {
+    app.listen(3000, () => {
+      console.log("Running on http://localhost:3000");
+    });
 
-		app.use(bodyParser.urlencoded({extended: true}));
-		app.use('/static', express.static('static'));
-		app.set('view engine', 'ejs');
-		// app.set('views', __dirname + 'views');
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use('/static', express.static('static'));
+    app.set('view engine', 'ejs');
+    // app.set('views', __dirname + 'views');
 
-		// home view
-		app.get('/', (req, res) => {
-			locals = {};
+    // home view
+    app.get('/', (req, res) => {
+      locals = {};
 
-			db.collection('posts').find().toArray((err, results) => {
-				if (err) return console.log(err);
-				locals.posts = results;
+      db.collection('posts').find().toArray((err, results) => {
+        if (err) return console.log(err);
+        locals.posts = results;
 
-				if (req.query.edit === "") {
-					db.collection('posts').findOne(
-						{_id: ObjectId(req.query.id)},
-						(err, result) => {
-							if (err) return console.log(err);
-							locals.post = result;
+        if (req.query.edit === "") {
+          db.collection('posts').findOne(
+            {_id: ObjectId(req.query.id)},
+            (err, result) => {
+              if (err) return console.log(err);
+              locals.post = result;
 
-							res.render('home.ejs', locals);
-							// res.send('Hello World');
-							// res.sendFile(__dirname + '/blog/home.html');
-						}
-					);
-				} else {
-					res.render('home.ejs', locals);
-				}
+              res.render('home.ejs', locals);
+              // res.send('Hello World');
+              // res.sendFile(__dirname + '/blog/home.html');
+            }
+          );
+        } else {
+          res.render('home.ejs', locals);
+        }
 
-			});
-		});
+      });
+    });
 
-		// create post
-		app.post('/new', (req, res) => {
-			db.collection('posts').save(req.body, (err, result) => {
-				if (err) return console.log(err);
-				res.redirect('/');
-			});
-		});
+    // create post
+    app.post('/new', (req, res) => {
+      db.collection('posts').save(req.body, (err, result) => {
+        if (err) return console.log(err);
+        res.redirect('/');
+      });
+    });
 
-		// edit post
-		app.post('/edit', (req, res) => {
-			db.collection('posts').update(
-				{_id: ObjectId(req.body.id)},
-				req.body,
-				(err, result) => {
-					if (err) return console.log(err);
-					res.redirect('/');
-				}
-			);
-		});
+    // edit post
+    app.post('/edit', (req, res) => {
+      db.collection('posts').update(
+        {_id: ObjectId(req.body.id)},
+        req.body,
+        (err, result) => {
+          if (err) return console.log(err);
+          res.redirect('/');
+        }
+      );
+    });
 
-		// delete post
-		app.post('/delete', (req, res) => {
-			db.collection('posts').remove(
-				{_id: ObjectId(req.body.id)}
-			);
-			res.redirect('/');
-		});
-	} else {
-		console.log("Error connecting to DB.");
-	}
+    // delete post
+    app.post('/delete', (req, res) => {
+      db.collection('posts').remove(
+        {_id: ObjectId(req.body.id)}
+      );
+      res.redirect('/');
+    });
+  } else {
+    console.log("Error connecting to DB.");
+  }
 });
